ce58d0676444f02aace2969459e436fa8e291ebe
debarpannotcreator <debarpannotcreator@gmail.com>
2026-07-29 05:18:50 +0000

    comparision in js

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+//console.log(a>b)
+//console.log(a<b)
+//console.log(a>=b)
+//console.log(a<=b)
+//console.log(a!=b)
+//console.log(a==b) these all are basic comparisions
+
+console.log("2">1) // ye 2 ko number me convert kar dega 
+
+
+console.log(null>0);
+console.log(null>=0);// ye true diya lakin baki dono false .. reason is quality check "==" and comparision "> < >= <=" works diffrently comparision converts null to a number that is "0" 
+console.log(null==0);
+
+// === also knoen as strict check
+
+console.log("2"==2);// true because ye convert kar deta hai str ko num me 
+console.log("2"===2);// false because ye zada strictly check karta hai toh data type same hai ki nhi wo bhe dekh leta hai .
+
+
+
